2f94e8943a9d67476d7b129e9ed6a506e6fbebd7
xcixor <pndungu54@gmail.com>
2023-08-30 11:06:40 +0300

    feat(auth): add next auth with google (typescript unoptimized)

---
+++ utils/db.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnected: boolean = false; // track the connection
+
+export const connectToDB = async (): Promise<void> => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
