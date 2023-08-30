3e5b68daad2073eaf516eab63cf4bbb76624949a
xcixor <pndungu54@gmail.com>
2023-08-30 15:56:40 +0300

    chore(crud): add prompts crud

---
+++ components/Profile.tsx
@@ -0,0 +1,40 @@
+import PromptCard from "./PromptCard";
+import { POST } from "@utils/reusablePropTypes";
+
+type ProfileProps = {
+  name: string;
+  desc: string;
+  data: POST[];
+  handleDelete: (params: any) => void;
+  handleEdit: (params: any) => void;
+};
+
+const Profile = ({
+  name,
+  desc,
+  data,
+  handleEdit,
+  handleDelete,
+}: ProfileProps) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="desc text-left">{desc}</p>
+
+      <div className="mt-10 prompt_layout">
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
